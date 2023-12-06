63afaac09b6df7fda4862050a98ecb6ee7f7e2bf
Rukky <abrukevwe@gmail.com>
2023-12-06 07:30:48 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import './styles/App.css';
 import Dice from './components/Dice';
+import { useState } from 'react';
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6), isHeld: false
+      })
+    }
+    return newDice;
+  };
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElement = dice.map(dice => (
+    <Dice
+      value={dice.value}
+      isHeld={dice.isHeld}
+    />
+  ))
+
   return (
     <main className="dice-bg">
       <div className="dice-container">
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
-          <Dice value = "1" />
+        {diceElement}
       </div>
+      <button className="roll-btn" onClick={rollDice}>Roll</button>
     </main>
   );
-}
+};
 
 export default App;
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,6 +1,10 @@
 function Dice(props) {
+    const isHeldStyle = {
+        backgroundColor: props.isHeld ? "#59E391" : "#fff"
+    }
+
     return (
-        <div className="dice-face">
+        <div className="dice-face" style={isHeldStyle}>
             <h2 className="dice-number">{props.value}</h2>
         </div>
     )
